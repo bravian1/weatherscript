2913f2e156a1844f11ba6f40febb6d473ca5b175
bravian1 <nyatorobravian@gmail.com>
2024-07-14 17:07:33 +0300

    feat: make the prompt for the ai be more fun oriented

--- main.go
+++ main.go
@@ -82,7 +82,18 @@ func geminiWrapper(name string) []byte {
 	}()
 	model := client.GenerativeModel("gemini-1.5-flash")
 
-	msg := "My name is " + name + ". Tell a joke first. teach me something small in a sentence about golang. Analyze the provided JSON weather data and create a meaningful weather forecast summary for the location and date specified. be funny Focus on the likelihood of rain, temperature range, and provide suggestions on what to wear and when to return home based on the weather conditions. Use natural language and make the forecast easy to understand." + getWeather()
+
+	msg := "My name is " + name + ". Tell a joke first. Then, provide a brief Go language tip. Next, analyze the provided JSON weather data and create a meaningful weather forecast summary for the location and date specified. The forecast should be:\n\n" +
+		"1. Personalized with the recipient's name and location.\n" +
+		"2. Include a joke at the beginning.\n" +
+		"3. Mention the likelihood of rain.\n" +
+		"4. Specify the temperature range.\n" +
+		"5. Provide suggestions on what to wear in the morning, afternoon, and evening.\n" +
+		"6. Use natural language and make the forecast easy to understand.\n" +
+		"7. Add emojis to make it visually appealing.\n" +
+		"8. If possible suggest an activity that can be done in kisumu if one does get out of work early.\n" +
+		"9. Conclude with a positive and engaging note. eg best regards CloudNine Forecasts by bravian\n" +
+		getWeather()
 	cs := model.StartChat()
 	resp, err := cs.SendMessage(ctx, genai.Text(msg))
 	if err != nil {
